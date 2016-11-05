eef5fac3c14b5819361e62fa3a2fb7433d46edf4
unscrambleor1 <unscrambleor@gmail.com>
2016-11-05 19:33:16 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -263,7 +263,7 @@ function create_config( )
     "Block",
     "InGroups",
     },
-    sudo_users = {123229845,},--Sudo users
+    sudo_users = {123229845,147876188,},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 ]],
